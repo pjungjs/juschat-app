d77c2d91cffb5ce51624e49c2680ad44a3441c08
pjungjs <pjungjs@gmail.com>
2023-05-19 07:43:12 -0400

    FE_updated footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,33 +4,11 @@ import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 function Footer() {
   return (
     <div className="bg-green-100 text-center lg:text-left">
-      <div className="flex p-6 items-center justify-center border-b-2 border-gray-400">
-        <div className="mr-12">
-          <span>Get connected with the developer:</span>
-        </div>
-        <div className="flex justify-center">
-          <a href="https://www.linkedin.com/in/jinseok-jung/" 
-            className="mr-6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiFillLinkedin className="h-7 w-7 text-black hover:text-blue-700" />
-          </a>
-          <a href="https://github.com/pjungjs" 
-            className="mr-6"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiFillGithub className="h-7 w-7 text-black hover:text-blue-700" />
-          </a>
-        </div>
-      </div>
-
-      <div className="mx-6 py-10 text-center md:text-left">
+      <div className="mx-6 py-10 text-center">
         <div className="grid-1 grid gap-11 md:grid-cols-2 lg:grid-cols-4">
-          <div className="">
+          <div className="justify-center">
             <Link to="/">
-              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase md:justify-start hover:underline">
+              <h6 className="mb-4 flex items-center justify-center font-semibold uppercase hover:underline">
                 JusChat App
               </h6>
             </Link>
@@ -43,29 +21,46 @@ function Footer() {
             </p>
           </div>
           <div>
-            <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
+            <h6 className="mb-4 flex justify-center font-semibold uppercase">
               Useful links
             </h6>
-            <p>
+            <p className="text-neutral-600 pb-2 hover:underline">
               <a href="https://github.com/pjungjs/juschat-app"
-                className="text-neutral-600 hover:underline"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 Project's Repo
               </a>
             </p>
+            <p className="text-neutral-600 pb-2 hover:underline">
+              <Link to="/about">
+                About
+              </Link>
+            </p>
           </div>
           <div>
-            <Link to="/about">
-              <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start hover:underline">
-                About
-              </h6>
-            </Link>
+            <h6 className="mb-4 flex justify-center font-semibold uppercase hover:underline">
+              Let's connect!
+            </h6>
+            <div className="flex justify-center">
+              <a href="https://www.linkedin.com/in/jinseok-jung/" 
+                className="mr-6"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillLinkedin className="h-7 w-7 text-black hover:text-blue-700" />
+              </a>
+              <a href="https://github.com/pjungjs" 
+                className="mr-6"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub className="h-7 w-7 text-black hover:text-blue-700" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
-
       <div className="bg-green-200 p-6 text-center">
         <span>© 2023 Copyright: </span>
         <span className="font-semibold">JusChat</span>
